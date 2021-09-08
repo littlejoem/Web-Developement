9f32a6a87cae06d2267319991b07b9cc0c99769a
littlejoem <malyshkz79@gmail.com>
2021-09-08 06:46:33 +0200

    Lesson 182. Adding Event Listeners with jQuery

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,1 +1,9 @@
-$("h1").css("color", "red");
+$("h1").addClass("big-title");
+
+$("h1").click(function() {
+  $("h1").css("color", "yellow")
+});
+
+$("input").keydown(function(event) {
+  console.log(event.key);
+});
