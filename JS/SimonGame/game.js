3db1beaf4b519d7ee37bd0ca7bc17aebf2dd26ab
littlejoem <malyshkz79@gmail.com>
2021-09-09 11:09:22 +0200

    Lesson 193. Step 3 - Show the Sequence to the User with Animations and Sounds

--- JS/SimonGame/game.js
+++ JS/SimonGame/game.js
@@ -1,1 +1,19 @@
 
+var gamePattern = [];
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  return randomNumber;
+}
+
+var randomChosenColour = buttonColours[nextSequence()];
+
+gamePattern.push(randomChosenColour);
+
+$("#" + randomChosenColour).fadeOut(100).fadeIn(50);
+
+$("div .btn").click(function() {
+  var audio = new Audio("sounds/" + $(this).attr("id") + ".mp3");
+  audio.play();
+})
