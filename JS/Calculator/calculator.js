c740997f2bd5fa1b1d0e8f58eea35b47fc16a237
littlejoem <malyshkz79@gmail.com>
2021-09-21 14:21:16 +0200

    Lesson 237. Solution to the BMI Routing Challenge

--- JS/Calculator/calculator.js
+++ JS/Calculator/calculator.js
@@ -20,6 +20,19 @@ app.post("/", (req, res) => {
   res.send("The result of calculator: " + result);
 });
 
+app.get('/bmicalculator', (req, res) => {
+  res.sendFile(__dirname + '/bmiCalculator.html');
+});
+
+app.post('/bmicalculator', (req, res) => {
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+
+  var bmi = weight / (height * height);
+
+  res.send('The BMI result is: ' + bmi);
+})
+
 app.listen(port, () => {
   console.log("Listen port " + port);
 });
