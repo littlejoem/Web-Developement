b8ad8c593ac3aa23135813b4e43b97c2615d44ed
littlejoem <malyshkz79@gmail.com>
2021-09-21 06:16:06 +0200

    Lesson 235. Processing Post Requests with Body Parser

--- JS/Calculator/calculator.js
+++ JS/Calculator/calculator.js
@@ -1,12 +1,25 @@
 
 const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+app.post("/", (req, res) => {
+
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+
+  var result = num1 + num2;
+
+  res.send("The result of calculator: " + result);
+});
+
 app.listen(port, () => {
   console.log("Listen port " + port);
-})
+});
