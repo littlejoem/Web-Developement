63b68feddfe4d65fc666a395ce378f124e53fa82
littlejoem <malyshkz79@gmail.com>
2021-09-27 06:14:23 +0200

    Lesson 244. How to Parse JSON

--- JS/WeatherProject/app.js
+++ JS/WeatherProject/app.js
@@ -7,15 +7,17 @@ app.get('/', (req, res) => {
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?q=New%20York&units=metric&lang=de&appid=1cfd9ddca757c0f858f9287c9e198dda';
   https.get(url, (response) => {
+    console.log('statusCode:' , response.statusCode);
+    console.log('header:', response.header);
 
-    console.log(response);
-
-  //   console.log('statusCode:' , response.statusCode);
-  //   console.log('header:', response.header);
-  //
-  //   response.on('data', (d) => {
-  //     process.stdout.write(d);
-  //   });
+    response.on('data', (d) => {
+      // process.stdout.write(d);
+      const weatherData = JSON.parse(d);
+      const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description
+      console.log(temp);
+      console.log(weatherDescription);
+    });
   // }).on('error', (e) => {
   //   console.log(e);
   })
