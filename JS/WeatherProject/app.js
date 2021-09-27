099c8e12a9f57cc1a73f2b2b0d7487b3d557802d
littlejoem <malyshkz79@gmail.com>
2021-09-27 05:50:28 +0200

    Lesson 243. Making GET Requests with the Node HTTPS Module

---
+++ JS/WeatherProject/app.js
@@ -0,0 +1,31 @@
+
+const express = require("express");
+const app = express();
+const https = require("https");
+
+app.get('/', (req, res) => {
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=New%20York&units=metric&lang=de&appid=1cfd9ddca757c0f858f9287c9e198dda';
+  https.get(url, (response) => {
+
+    console.log(response);
+
+  //   console.log('statusCode:' , response.statusCode);
+  //   console.log('header:', response.header);
+  //
+  //   response.on('data', (d) => {
+  //     process.stdout.write(d);
+  //   });
+  // }).on('error', (e) => {
+  //   console.log(e);
+  })
+
+
+  res.send("Server is up and running.");
+});
+
+
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000.");
+});
