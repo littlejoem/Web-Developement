2a84e48227f52bc6670d47517307d1c1de353243
littlejoem <malyshkz79@gmail.com>
2021-09-28 06:57:16 +0200

    Lesson 246. Using Body Parser to Parse POST Requests to the Server

--- JS/WeatherProject/app.js
+++ JS/WeatherProject/app.js
@@ -1,11 +1,21 @@
 
-const express = require("express");
+const express = require('express');
 const app = express();
-const https = require("https");
+const https = require('https');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&lang=en&appid=1cfd9ddca757c0f858f9287c9e198dda';
+});
+
+app.post('/', (req, res)=> {
+  const query = req.body.cityName;
+  const unit = 'metric';
+  const apiKey = '1cfd9ddca757c0f858f9287c9e198dda';
+  const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + query + '&units=' + unit + '&lang=en&appid=' + apiKey;
   https.get(url, (response) => {
     console.log('statusCode:' , response.statusCode);
     console.log('header:', response.header);
@@ -18,12 +28,13 @@ const url = 'https://api.openweathermap.org/data/2.5/weather?q=London&units=metr
       const weatherConditionIcon = weatherData.weather[0].icon;
       const imageURL = 'http://openweathermap.org/img/wn/' + weatherConditionIcon + '@2x.png';
       res.write('<p>The weather is currently ' + weatherDescription + '</p>');
-      res.write('<h1>The temperature in New York is ' + temp + ' degrees Celcius.</h1>');
+      res.write('<h1>The temperature in ' + query + ' is ' + temp + ' degrees Celcius.</h1>');
       res.write('<img src=' + imageURL + '>');
       res.send();
     });
   });
-});
+})
+
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000.");
