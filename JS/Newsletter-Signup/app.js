a6ffec9dfdb3f1f53f6b46604c0b65f844781b0a
littlejoem <malyshkz79@gmail.com>
2021-10-02 06:40:05 +0200

    Lesson 249. Posting Data to Mailchimp's Servers via their API

--- JS/Newsletter-Signup/app.js
+++ JS/Newsletter-Signup/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const request = require('request');
+const https = require('https');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -12,13 +13,48 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-  var firstName = req.body.fName;
-  var lastName = req.body.lName;
-  var email = req.body.email;
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
 
-  console.log(firstName, lastName, email);
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us5.api.mailchimp.com/3.0/lists/8d232db6f8";
+  const options = {
+    method: "POST",
+    auth: "littlejoem:ed1ccfac558621ee87f1a7438be6969d-us5"
+  };
+
+  const request = https.request(url, options, (response) => {
+    response.on("data", (data) => {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+
 });
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+// API key
+// ed1ccfac558621ee87f1a7438be6969d-us5
+
+//Audience ID
+// 8d232db6f8
