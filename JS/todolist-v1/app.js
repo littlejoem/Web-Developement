5e91c80672776bbe1a6f9e8c385e1c1e8aaf32d3
littlejoem <malyshkz79@gmail.com>
2021-10-18 07:22:17 +0200

    Lesson 268. Passing Data from Your Webpage to Your Server

--- JS/todolist-v1/app.js
+++ JS/todolist-v1/app.js
@@ -2,19 +2,34 @@ let express = require('express');
 let bodyParser = require('body-parser');
 let app = express();
 
+var items = [];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
-  var daysOfWeek = ['Sunday', 'Monday', 'Tusday', 'Wednsday', 'Thursday', 'Friday', 'Saturday'];
+  var option = {
+    weekday:  "long",
+    day: "numeric",
+    month: "long",
+  };
 
-  day = daysOfWeek[currentDay];
+  var day = today.toLocaleDateString('en-US', option);
+
   res.render('list', {
-    kindOfDay: day
+    kindOfDay: day,
+    newListItems: items,
   });
+});
 
+app.post('/', (req, res) => {
+  var item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect('/');
 });
 
 
