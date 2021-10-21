7baa32bc531d134ebbd54397ac156ae1b216bd6e
littlejoem <malyshkz79@gmail.com>
2021-10-21 07:24:10 +0200

    Lesson 288. Challenge 7

--- JS/ejs-challenge/app.js
+++ JS/ejs-challenge/app.js
@@ -15,11 +15,21 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get('/', (req, res) => {res.render('home', {startingContent: homeStartingContent})});
+app.get('/', (req, res) => {
+  res.render('home', {startingContent: homeStartingContent});
+});
 
-app.get('/about', (req, res) => {res.render('about', {aboutContent: aboutContent})});
+app.get('/about', (req, res) => {
+  res.render('about', {aboutContent: aboutContent});
+});
 
-app.get('/contact', (req, res) => {res.render('contact', {contactContent: contactContent})});
+app.get('/contact', (req, res) => {
+  res.render('contact', {contactContent: contactContent});
+});
+
+app.get('/compose', (req, res) => {
+  res.render('compose');
+});
 
 
 
